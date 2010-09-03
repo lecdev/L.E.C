7249265f74ed403c597bf61540595951fc87e7ad
Dave Stevens <dave@theinternationalgame.com>
2010-09-02 23:29:56 -0700

    early progress but not much to show

--- app.js
+++ app.js
@@ -29,3 +29,68 @@ function load_static_file(uri, response) {
 		});
 	});
 }
+
+var update_emitter = new events.EventEmitter();
+var storage = [];
+
+function get_updates() {
+  
+  // pull from couchDB or whatever document storage
+  
+  var i = storage.length;
+  sys.puts(i);
+  update_emitter.emit("updates",JSON.parse(storage[0]))
+
+/*  
+	var request = twitter_client.request("GET", "/1/statuses/public_timeline.json", {"host": "api.twitter.com"});
+
+	request.addListener("response", function(response) {
+		var body = "";
+		response.addListener("data", function(data) {
+			body += data;
+		});
+
+		response.addListener("end", function() {
+			var updates = JSON.parse(body);
+			if(updates.length > 0) {
+				tweet_emitter.emit("updates", updates);
+			}
+		});
+	});
+
+	request.close();
+	*/
+}
+
+setInterval(get_updates, 5000);
+
+http.createServer(function(request, response) {
+    var uri = url.parse(request.url).pathname;
+    if(uri === "/stream") {
+
+    	var listener = update_emitter.addListener("updates", function(updates) {
+    		response.sendHeader(200, { "Content-Type" : "text/plain" });
+    		response.write(JSON.stringify(updates));
+    		response.close();
+
+    		clearTimeout(timeout);
+    	});
+
+    	var timeout = setTimeout(function() {
+    		response.sendHeader(200, { "Content-Type" : "text/plain" });
+    		response.write(JSON.stringify([]));
+    		response.close();
+
+    		update_emitter.removeListener(listener);
+    	}, 10000);
+
+    } else if (uri === "/push"){
+      // write to couchdb or whatever document storage here
+      storage.push("{'message':'pushed!'}");
+      sys.puts("done a push");
+    } else {
+    	load_static_file(uri, response);
+    }
+}).listen(8080);
+
+sys.puts("Server running at http://localhost:8080/");
--- js/lec.js
+++ js/lec.js
@@ -1,12 +1,21 @@
-function(){
-  var lec, L = {
+(function(){
+  var lec, update_list = $("#updates"), L = {
     init: function() {
-      this.m.hideGracefulElements();
+      //this.m.hideGracefulElements();
+      setTimeout(this.m.load_updates,1000);
     },
     m: {
       hideGracefulElements: function() {
         $(".graceful").hide();
-      }
+      },
+  		load_updates: function() {
+  			$.getJSON("/stream", function(updates) {
+  				$.each(updates, function() {
+  					$("<li>").html(this.text).prependTo(update_list);
+  				});
+  				L.m.load_updates();
+  			});
+  		}      
     }
   };
   $(document).ready(function(){
